a5ae4805d101eecac53bf46583272f71cb9b0079
Leonardo Benvenutti <140168119+8462leo@users.noreply.github.com>
2024-03-06 18:57:02 -0300

    Epifania
    Epifania

---
+++ principal.js
@@ -0,0 +1,40 @@
+/*INDEX*/
+
+ScrollReveal().reveal('.h1_somos', {origin:'left', duration: 750, distance:'250px', easing:'ease-out', reset:true});
+
+ScrollReveal().reveal('.conteudo_somos', {origin:'bottom', duration: 1000, distance:'250px', easing:'ease-out', reset:true});
+
+ScrollReveal().reveal('.div_lucas', {origin:'bottom', duration: 1000, distance:'250px', easing:'ease-out', reset:true});
+
+ScrollReveal().reveal('.div_camila', { origin:'bottom', duration: 1000, distance:'250px', easing:'ease-out', reset:true});
+
+ScrollReveal().reveal('.div_funcionario', {origin:'bottom', duration: 1000, distance:'250px', easing:'ease-out', reset:true});
+
+ScrollReveal().reveal('.maps', {origin:'bottom', duration: 1000, distance:'250px', easing:'ease-out', reset:true});
+
+ScrollReveal().reveal('.conteudo_somos', { duration: 2000,  easing:'ease-out', reset:true});
+
+/*PRODUTOS*/
+
+ScrollReveal().reveal('.foto_primeiro_produto', {origin:'bottom', duration: 2000, distance:'250px', easing:'ease-out', reset:true});
+
+ScrollReveal().reveal('.descrição_primeiro_produto', {origin:'bottom', duration: 1000, distance:'250px', easing:'ease-out', reset:true});
+
+
+/*PORTIFOLIO*/
+
+ScrollReveal().reveal('.fundo_portifolio', {origin:'bottom', duration: 1500, distance:'250px', easing:'ease-out',rotate: {x:'60px', z:'50px'} ,reset:true});
+
+/*TATTOO*/
+
+ScrollReveal().reveal('.fundo_Tattoo-1', {origin:'bottom', duration: 1500, distance:'250px', easing:'ease-out',rotate: {x:'60px', z:'50px'} ,reset:true});
+
+ScrollReveal().reveal('.foto_primeiro_produto', {origin:'bottom', duration: 1000, distance:'250px', easing:'ease-out', reset:true});
+
+ScrollReveal().reveal('.descrição_primeiro_produto', {origin:'bottom', duration: 1000, distance:'250px', easing:'ease-out', reset:true});
+
+/*CONTATO*/
+
+ScrollReveal().reveal('.contato_barbearia', {origin:'bottom', duration: 2000, distance:'250px', easing:'ease-out',rotate: {x:'60px', z:'50px'} ,reset:true});
+
+ScrollReveal().reveal('.contato_tatuagem', {origin:'bottom', duration: 2000, distance:'250px', easing:'ease-out',rotate: {x:'60px', z:'50px'} ,reset:true});
